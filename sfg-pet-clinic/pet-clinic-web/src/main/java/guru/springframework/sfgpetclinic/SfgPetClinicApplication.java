823214dc26ff43165f4059ffe955749f4fc866b4
Kevin <mcgonagle.kev@gmail.com>
2020-11-08 16:54:23 +0000

    Refactoring to introduce Maven modules, Closes #1.

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SgfPetClinicApplication {
+public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SgfPetClinicApplication.class, args);
+		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 
 }
